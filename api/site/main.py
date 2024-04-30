d3c6f9f18c13d08429ee2ef03c8fa91d9d53d64d
AliHaine <alitr57120@gmail.com>
2024-04-30 12:27:24 +0200

    Udemy python day 31 without exersice

--- api/site/main.py
+++ api/site/main.py
@@ -1,20 +1,27 @@
+import pandas as pd
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+stations = (pd.read_csv("../../jupyter/data_small/stations.txt", skiprows=17))
+stations = stations[["STAID", "STANAME                                 "]]
 
 @app.route("/")
 def home():
-	return render_template("home.html")
+	return render_template("home.html", data=stations.to_html())
 
 
-@app.route("/api/v1/<station>/<date>")
-def about(station, date):
-	return {"station": station,
-			"date": date,
-			"temperature": 50
-			}
+@app.route("/api/v1/yearly/<station>/<date>")
+def yearly(station, date):
+	filename = "../../jupyter/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+	df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+	return df[df["    DATE"].astype(str).str.startswith(date)].to_html()
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+	filename = "../../jupyter/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+	df = pd.read_csv(filename, skiprows=20)
+	return df.to_html()
 
 if __name__ == "__main__":
 	app.run(debug=True)
