96a386950a5f3ec2de31ff54e4dae7b9e5aee901
AliHaine <alitr57120@gmail.com>
2024-05-14 15:33:10 +0200

    udemy day 57

--- django/res_menu/res_menu/urls.py
+++ django/res_menu/res_menu/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	path("", views.ItemMenuList.as_view(), name="home"),
-	path("menu_detail/<pk>", views.ItemDetail.as_view(), name="detail"),
+	path("menu_detail/<int:pk>/", views.ItemDetail.as_view(), name="detail"),
 ]
